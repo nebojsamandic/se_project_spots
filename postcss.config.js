1efb523d3a7a0a1d1fcef386d9add1901532e5b2
Nebojsa Mandic <skymandic@gmail.com>
2025-06-01 23:28:26 -0400

    final update

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
+// Connect plugins to the file
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
 module.exports = {
+  // Connect plugins to PostCSS
   plugins: [
-    require('autoprefixer'),
-    require('cssnano')({
-      preset: 'default'
-    })
-  ]
+    autoprefixer,
+    cssnano({ preset: "default" }), // set default minification settings
+  ],
 };
---
+++ src/scripts/api.js
@@ -0,0 +1,20 @@
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers
+    }).then(this._handleResponse);
+  }
+
+  _handleResponse(res) {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  }
+
+  // You can add other API methods here, like getUserInfo, addCard, etc.
+}
+
+export default Api;
